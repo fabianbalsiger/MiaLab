c6788b6818afb4649559db8e660845d557fffd95
GrafXP <m_pletscher@yahoo.de>
2015-10-12 23:20:26 +0200

    added helper function to calculate image histogram

--- helpers/imgHist.m
+++ helpers/imgHist.m
@@ -1,8 +1,19 @@
 function [ ] = imgHist( I )
-%IMGHIST Shows an image's histogram.
-%   IMGHIST(I) show the histogram of the image I.
-    
-    %figure;
-    %plot...
+    %IMGHIST Shows an image's histogram.
+    %IMGHIST(I) show the histogram of the image I.
+
+    [m,n] = size(I);
+    maxVal = max(max(I));
+
+    histoArr = zeros(maxVal);
+
+    for x = 1:m
+        for y = 1:n
+            histoArr(I(x,y)) = histoArr(I(x,y)) + 1;
+        end
+    end
+
+    figure;
+    plot(1:maxVal,histoArr);
 end
 
